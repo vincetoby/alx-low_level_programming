a9a5ee980c561c60c675b1d41c80eaee92542c61
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-07-14 12:14:58 -0700

    update 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+/*
+ * File: 3-print_alphabet.c
+ * Auth: Vincent T. Ekechukwu
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if(letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
