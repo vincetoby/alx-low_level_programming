98cfdffda0378f6ffe5198f6bb27225a83db9914
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-07-18 07:28:27 -0700

    update 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Vincent T. Ekechukwu
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints _putchar.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
