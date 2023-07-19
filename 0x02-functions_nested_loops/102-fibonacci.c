9b291eaef6dcf1abca6f9c8ac2079ae31e3e07db
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-07-19 13:31:05 -0700

    update 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	unsigned long int sum;
+	int i;
+
+	printf("%lu, ", a);
+	for (i = 1; i < 50; i++)
+	{
+		printf("%lu, ", b);
+		sum = a + b;
+		a = b;
+		b = sum;
+	}
+	printf("\n");
+	return (0);
+}
