7b02a70be8520bf78d95d1855ac924ee5e47b8f9
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-07-19 14:23:02 -0700

    update 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int sum;
+	int even = 2;
+	int lim = 4000000;
+
+	while (sum <= lim)
+	{
+		sum = a + b;
+		if (sum % 2 == 0)
+		{
+			even = even + sum;
+		}
+		a = b;
+		b = sum;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
