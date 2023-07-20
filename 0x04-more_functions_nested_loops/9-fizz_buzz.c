938d9ead373ef4c342ea9020e41e38254d7108a3
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-07-20 14:53:44 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,18 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-                        printf("FizzBuzz");
+		if (i % 15 == 0)
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%d", i);i
-		if (num != 100)
+			printf("%d", num);
+
+		if (i != 100)
 			printf(" ");
 	}
 	printf("\n");
