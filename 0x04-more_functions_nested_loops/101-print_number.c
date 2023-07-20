51aa46e13c07d1edd60931c6546098b246df5fd5
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-07-20 15:47:55 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_number - Function that prints an integer.
+ * @n: parameter
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	long m;
+	int c;
+	long num;
+
+	num = n;
+	/* negatives */
+	if (num < 0)
+	{
+		num *= -1;
+		_putchar('-');
+	}
+
+	/* count up */
+	m = 1;
+	c = 1;
+	while (c)
+	{
+		if (num / (m * 10) > 0)
+			m *= 10;
+		else
+			c = 0;
+	}
+
+	/* count down */
+	while (num >= 0)
+	{
+		if (m == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / m % 10) + '0');
+			m /= 10;
+		}
+	}
+}
