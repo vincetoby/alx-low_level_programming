8a2ed3c19923d551c1543c516143aa83e5df2c27
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-07-24 11:44:23 -0700

    update 0x05-pointers_arrays_strings
    I updated the directory 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string to be printed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		counter++;
+	for (i = counter - 1; i >= 0; i--)
+		_putchar(s[i]);
+}
