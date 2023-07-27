4e09a7d65cb7dfbe5a657bc834cd47fad86386a1
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-07-27 05:18:10 -0700

    update 0x06-pointers_arrays_strings
    I updated the directory 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes most of the words in a string.
- * @s: analized string.
+ * cap_string - capitalizes first letter of the words in a string.
+ * @s: string to be analyzed.
  *
  * Return: String with all words capitalized.
  */
@@ -10,29 +10,23 @@
 char *cap_string(char *s)
 {
 	int i, j;
-	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int a[13] = {' ', '\t', '\n', ',', ';', '.',
+'!', '?', '"', '(', ')', '{', '}'};
 
-	i = 0;
-	while (*(s + i) != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 		{
-			if (i == 0)
+			s[i] = s[i] - 32;
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == a[j])
 			{
-				*(s + i) = *(s + i) - 32;
-			}
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-					{
-						*(s + i) = *(s + i) - 32;
-					}
-				}
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i] = s[i] - 32;
 			}
 		}
-	i++;
 	}
 	return (s);
 }
