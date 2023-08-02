feaf3dce47470a7b73ff9317e367e25195820dc5
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-02 12:40:49 -0700

    update 0x08-recursion
    I updated the directory 0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * sqrt - Makes possible to evaluate from 1 to n
+ * sqrt2 - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
  *
@@ -9,13 +9,13 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int sqrt(int a, int b)
+int sqrt2(int a, int b)
 {
 	if (b * b == a)
 		return (b);
 	else if (b * b > a)
 		return (-1);
-	return (sqrt(a, b + 1));
+	return (sqrt2(a, b + 1));
 }
 
 /**
@@ -28,5 +28,5 @@ int sqrt(int a, int b)
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	return (sqrt2(n, 1));
 }
