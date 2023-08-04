f6f2eb34f5ee6ef5bae44b46658f72831876a1dd
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-04 11:31:10 -0700

    0x0A-argc_argv
    I updated the directory 0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argv: pointer to string
+ * @argc: string count
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -4,4 +4,4 @@ PLEASE READ ME
 
     If you rename the program, it will print the new name, without having to compile it again
     You should not remove the path before the name of the program
-
+2. 1-args.c: prints the number of arguments passed into it.
