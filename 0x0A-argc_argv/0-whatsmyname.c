12409274c3d614c37b77de28dd65da8eb483c83a
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-04 10:53:25 -0700

    0x0A-argc_argv
    I updated the directory 0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  entry point that writes its name
+ * @argc: arguement count
+ * @argv: pointer to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
