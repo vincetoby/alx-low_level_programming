cd176848016ac73c7d35f9ad6b3f48e7da5a8561
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-07 15:21:17 -0700

    0x0B-malloc_free
    I updated the directory 0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 	count2 = 0;
 
 	if (s1 == NULL)
-		return (" ");
+		return ("");
 	if (s2 == NULL)
-		return (" ");
+		return ("");
 
 	for (i = 0; s1[i] != '\0'; i++)
 		count1++;
