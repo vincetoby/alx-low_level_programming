cc2b2df3a62b26b8a602bfa366765eef0114dc58
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-14 15:59:08 -0700

    0x0E-structures_typedef
    I updated the directory 0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,13 +26,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	d->name = malloc(sizeof(char) * len1 + 1);
-	if (name == NULL)
+	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
 	d->owner = malloc(sizeof(char) * len2 + 1);
-	if (owner == NULL)
+	if (d->owner == NULL)
 	{
 		free(d);
 		free(d->name);
