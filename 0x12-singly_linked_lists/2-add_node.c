2cb09d34a5089fedca07441e2e21d86ceeda9a05
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-24 13:50:41 -0700

    0x12-singly_linked_lists
    I updated the directory 0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,11 +15,43 @@ list_t *add_node(list_t **head, const char *str)
 	tempo = malloc(sizeof(list_t));
 	if (tempo == NULL)
 		return (NULL);
-	tempo->str = strdup(str);
+	tempo->str = _strdup(str);
 	while (str[counter] != '\0')
 		counter++;
 	tempo->len = counter;
 	tempo->next = *head;
 	*head = tempo;
 	return (tempo);
+}
+
+/**
+ * *_strdup - returns a pointer to allocated space in memory
+ * @str: string arg
+ * Return: pointer
+ */
+
+char *_strdup(const char *str)
+{
+	int i, j, count = 0;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	ptr = malloc(sizeof(char) * count + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for(j = 0; str[j] != '\0'; j++)
+	{
+		ptr[j] = str[j]; /* copy contents of str into ptr */
+	}
+	ptr[j] = '\0'; /* terminate with a null byte */
+	return (ptr);
 }
