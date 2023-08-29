fc80113538721d6eee5b2167410e0b3235b645b6
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-28 22:09:26 -0700

    0x13-more_singly_linked_lists
    I updated the directory 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,10 +12,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (!h->n)
-			printf("NULL\n");
-		else
-			printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		nodes++;
 	}
