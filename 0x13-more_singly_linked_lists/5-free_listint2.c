e468307646747440e14b1bad8c0389367ea1239f
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-29 11:13:48 -0700

    0x13-more_singly_linked_lists
    I updated the directory 0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a linked  list
+ * @head: double Pointer to linked list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp; /*temporary pointer to keep track of the next node*/
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)/* Loop while there are nodes remaining in the list*/
+	{
+		temp = *head->next; /*Store the next node in the temporary pointer*/
+		free(*head); /* free memory for the current node*/
+		*head = temp; /* head copies address of next node stored in temp*/
+				/* so now head points to next node on the list */
+	}
+	*head = NULL;
+}
