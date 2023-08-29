e4b66346945fbee5f5e39a81c8fdb7d1b4f2776d
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-28 22:44:54 -0700

    0x13-more_singly_linked_lists
    I updated the directory 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a linked list
+ * listint_len - counts the number of nodes
  * @h: pointer to first node
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t nodes = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		nodes++;
 	}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,10 +12,11 @@
  * Description: singly linked list node structure
  * 
  */
+
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
