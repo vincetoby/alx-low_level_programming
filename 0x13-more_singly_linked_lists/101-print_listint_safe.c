e1ceaf725429eb6aad072ba0b5e1ea61b1f0191d
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-30 06:08:14 -0700

    0x13-more_singly_linked_lists
    I updated the directory 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -20,6 +20,7 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
 		printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+		nodes++;
 	}
 	return (nodes);
 }
