dc32583e782e8029cdcebd47516b49673a709ba6
Vincent Ekechukwu <vincetobechukwu@gmail.com>
2023-08-31 11:25:07 -0700

    update 0x14-bit_manipulation
    I updated the directory 0x14-bit_manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,7 @@ int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif
